74accd263b85cc049702ec75840ebae0342970c7
hisaki-okamoto <hisaki.okamoto@tes.co.jp>
2026-08-17 17:09:35 +0900

    sample code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PokemonClient } from 'pokenode-ts';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('pokemon');
+  async ngOnInit () {
+    const api = new PokemonClient();
+
+const pokemon = await api.getPokemonByName('luxray');
+
+console.log(pokemon.name); // "luxray"
+console.log(pokemon.types.map((slot) => slot.type.name)); // ["electric"]
+  }
 }
