b1701666083db3535ea49564b6e825d9f40b7726
hisaki-okamoto <hisaki.okamoto@tes.co.jp>
2026-08-18 09:06:42 +0900

    フォルダの作成

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,21 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { PokemonClient } from 'pokenode-ts';
+import { Component, signal } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { PokemonClient, UtilityClient } from "pokenode-ts";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+	selector: "app-root",
+	imports: [RouterOutlet],
+	templateUrl: "./app.html",
+	styleUrl: "./app.css",
 })
 export class App {
-  protected readonly title = signal('pokemon');
-  async ngOnInit () {
-    const api = new PokemonClient();
+	protected readonly title = signal("pokemon");
+	async ngOnInit() {
+		const api = new PokemonClient();
 
-const pokemon = await api.getPokemonByName('luxray');
+		const pokemon = await api.getPokemonSpeciesByName("luxray");
 
-console.log(pokemon.name); // "luxray"
-console.log(pokemon.types.map((slot) => slot.type.name)); // ["electric"]
-  }
+		console.log(pokemon.names); // "luxray"
+		// console.log(pokemon.types.map((slot) => slot.type.name)); // ["electric"]
+
+		const api2 = new UtilityClient();
+
+		const japanese = await api2.getLanguageByName("");
+
+		console.log(japanese); // true
+
+		const kairosu = await api.getPokemonSpeciesById(127);
+		console.log(kairosu.names);
+	}
 }
