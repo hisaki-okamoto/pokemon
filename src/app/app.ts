2bfeb9dc841af1594e83ce457a061c52ce4c7c5d
hisaki-okamoto <hisaki.okamoto@tes.co.jp>
2026-08-18 14:01:50 +0900

    ページ変更ボタンの追加 ()の付け忘れの修正

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,20 +11,14 @@ import { PokemonClient, UtilityClient } from "pokenode-ts";
 export class App {
 	protected readonly title = signal("pokemon");
 	async ngOnInit() {
-		const api = new PokemonClient();
-
-		const pokemon = await api.getPokemonSpeciesByName("luxray");
-
-		console.log(pokemon); // "luxray"
-		// console.log(pokemon.types.map((slot) => slot.type.name)); // ["electric"]
-
-		const api2 = new UtilityClient();
-
-		const japanese = await api2.getLanguageByName("");
-
-		console.log(japanese); // true
-
-		const kairosu = await api.getPokemonSpeciesById(127);
-		console.log(kairosu.names);
+		// const api = new PokemonClient();
+		// const pokemon = await api.getPokemonSpeciesByName("luxray");
+		// console.log(pokemon); // "luxray"
+		// // console.log(pokemon.types.map((slot) => slot.type.name)); // ["electric"]
+		// const api2 = new UtilityClient();
+		// const japanese = await api2.getLanguageByName("");
+		// console.log(japanese); // true
+		// const kairosu = await api.getPokemonSpeciesById(127);
+		// console.log(kairosu.names);
 	}
 }
--- src/list-page/list-page.ts
+++ src/list-page/list-page.ts
@@ -3,8 +3,10 @@ type Pokemon = {
 	name: string;
 };
 
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+// biome-ignore lint/style/useImportType: <explanation>
+import { ChangeDetectorRef, Component, inject } from "@angular/core";
+// biome-ignore lint/style/useImportType: <explanation>
+import { ActivatedRoute, Router, RouterOutlet } from "@angular/router";
 import { PokemonClient } from "pokenode-ts";
 
 @Component({
@@ -18,20 +20,59 @@ export class List {
 
 	pokemons: Pokemon[] = [];
 	start = 1;
-	count: number[] = Array.from({ length: 5 }, (_, i) => this.start + i);
+	count: number[] = [];
+
+	private router = inject(Router);
+	constructor(
+		private cdr: ChangeDetectorRef,
+		private route: ActivatedRoute,
+	) {}
 
 	async getList() {
-		for (let i = 0; i < 5; i++) {
+		this.count = Array.from({ length: 5 }, (_, i) => this.start + i);
+		this.pokemons.length = 0;
+		for (let i = 0; i < this.count.length; i++) {
 			const getData = await this.api.getPokemonSpeciesById(this.count[i]);
 			this.pokemons.push({
 				id: getData.id,
 				name: String(getData.names[9].name),
 			});
 		}
+		this.cdr.markForCheck();
 	}
 
-	async ngAfterViewInit() {
-		await this.getList();
-		console.log(this.pokemons);
+	backPage() {
+		this.start -= this.count.length;
+		this.toParam();
+	}
+	frontPage() {
+		this.start += this.count.length;
+		this.toParam();
+	}
+
+	toParam() {
+		this.router.navigate([""], {
+			queryParams: {
+				start: this.start,
+			},
+		});
+	}
+
+	ngOnInit() {
+		this.route.queryParams.subscribe(async (params) => {
+			if (params) {
+				if (
+					!Number.isNaN(Number(params["start"])) &&
+					Number(params["start"]) > 0
+				) {
+					this.start = Number(params["start"]);
+				} else {
+					this.start = 1;
+					// this.toParam();
+				}
+				await this.getList();
+				this.cdr.markForCheck();
+			}
+		});
 	}
 }
