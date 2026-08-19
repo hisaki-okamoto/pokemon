b01dc0a971c4b3a7037a11b9843b99b6fa1f3ffb
hisaki-okamoto <hisaki.okamoto@tes.co.jp>
2026-08-19 10:55:37 +0900

    タイプごとの色選択の実装

---
+++ src/app/utils/typeColor.ts
@@ -0,0 +1,39 @@
+export function typeColor(type: string): string {
+  if (type === "くさ") {
+    return "#00ff00";
+  } else if (type === "ほのお") {
+    return "#ff0000";
+  } else if (type === "みず") {
+    return "#00c0ff";
+  } else if (type === "でんき") {
+    return "#ffff00";
+  } else if (type === "じめん") {
+    return "#b38900";
+  } else if (type === "いわ") {
+    return "#968c7e";
+  } else if (type === "こおり") {
+    return "#c0ffff";
+  } else if (type === "かくとう") {
+    return "#ffa000";
+  } else if (type === "どく") {
+    return "#803080";
+  } else if (type === "ひこう") {
+    return "#00ffff";
+  } else if (type === "エスパー") {
+    return "#ff7070";
+  } else if (type === "むし") {
+    return "#40a040";
+  } else if (type === "ゴースト") {
+    return "#604060";
+  } else if (type === "ドラゴン") {
+    return "#5030ff";
+  } else if (type === "あく") {
+    return "#222222";
+  } else if (type === "はがね") {
+    return "#aaaaaa";
+  } else if (type === "フェアリー") {
+    return "#ffaaf0";
+  } else {
+    return "#dddddd";
+  }
+}
